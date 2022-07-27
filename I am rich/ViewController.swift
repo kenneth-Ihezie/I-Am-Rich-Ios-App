fc1d79cb231fc6674dedf74866464005155bfb60
collins ihezie <collinsihezie6@gmail.com>
2022-07-27 11:45:31 +0100

    initial commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Gava Developer on 22/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
